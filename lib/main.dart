22b6716ab255ab6497b9937a51be12985450b48f
Pavithra Nandhini Ramachandran <pavithranandhini.ramachandran@syncfusion.com>
2021-09-27 12:20:29 +0530

    flut-5452-[feature][flutter]: Ignore file added

--- lib/main.dart
+++ lib/main.dart
@@ -18,7 +18,7 @@ class SynchronizedTrackball extends StatelessWidget {
 }
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key}) : super(key: key);
+  MyHomePage({Key? key}) : super(key: key);
 
   @override
   _MyHomePageState createState() => _MyHomePageState();
