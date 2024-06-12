c0b2b997a7b67b1ee19f0ecd4468b0252180d86d
LokeshPalani <lokesh.palani@syncfusion.com>
2024-06-13 00:03:40 +0530

    FLUT-890889-[others][flutter]: Modified kb sample

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:syncfusion_flutter_charts/charts.dart';
 
-void main() => runApp(SynchronizedTrackball());
+void main() => runApp(const SynchronizedTrackball());
 
 class SynchronizedTrackball extends StatelessWidget {
+  const SynchronizedTrackball({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -12,13 +14,13 @@ class SynchronizedTrackball extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(),
+      home: const MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key? key}) : super(key: key);
+  const MyHomePage({super.key});
 
   @override
   _MyHomePageState createState() => _MyHomePageState();
@@ -27,32 +29,46 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
+    return const SafeArea(
       child: Scaffold(
-          backgroundColor: Colors.white,
-          body: SingleChildScrollView(
-            scrollDirection: Axis.vertical,
-            child: Column(children: <Widget>[
-              Container(
+        backgroundColor: Colors.white,
+        body: SingleChildScrollView(
+          scrollDirection: Axis.vertical,
+          child: Column(
+            children: <Widget>[
+              SizedBox(
                 height: 280,
                 child: FirstChart(),
               ),
-              Container(
+              SizedBox(
                 height: 280,
                 child: SecondChart(),
               ),
-            ]),
-          )),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
 
+// Trackball behavior instances for the two charts.
 TrackballBehavior trackBall1 =
     TrackballBehavior(enable: true, activationMode: ActivationMode.singleTap);
 TrackballBehavior trackBall2 =
     TrackballBehavior(enable: true, activationMode: ActivationMode.singleTap);
 
+// Controllers for accessing and manipulating the series in the charts.
+ChartSeriesController? _secondChartController;
+ChartSeriesController? _firstChartController;
+
+// Variables to store the trackball positions for synchronization.
+Offset? _firstPosition;
+Offset? _secondPosition;
+
 class FirstChart extends StatefulWidget {
+  const FirstChart({super.key});
+
   @override
   State<StatefulWidget> createState() {
     return FirstChartState();
@@ -60,39 +76,64 @@ class FirstChart extends StatefulWidget {
 }
 
 class FirstChartState extends State<FirstChart> {
+  // Declaring a field to check whether the user is interacting with the chart.
+  bool _isInteractive = false;
+
   @override
   Widget build(BuildContext context) {
     return SfCartesianChart(
-        onChartTouchInteractionMove: (ChartTouchInteractionArgs args) {
-          trackBall2.show(args.position.dx, args.position.dy, 'pixel');
-        },
-        backgroundColor: Colors.white,
-        primaryXAxis: CategoryAxis(),
-        title: ChartTitle(text: 'Chart 1'),
-        trackballBehavior: trackBall1,
-        series: <LineSeries<SalesData, String>>[
-          LineSeries<SalesData, String>(
-              dataSource: <SalesData>[
-                SalesData('Jan', 21),
-                SalesData('Feb', 24),
-                SalesData('Mar', 35),
-                SalesData('Apr', 38),
-                SalesData('May', 54),
-                SalesData('Jun', 21),
-                SalesData('Jul', 24),
-                SalesData('Aug', 35),
-                SalesData('Sep', 38),
-                SalesData('Oct', 54),
-                SalesData('Nov', 38),
-                SalesData('Dec', 54)
-              ],
-              xValueMapper: (SalesData sales, _) => sales.year,
-              yValueMapper: (SalesData sales, _) => sales.sales)
-        ]);
+      // Set to true when user touch on the chart.
+      onChartTouchInteractionDown: (ChartTouchInteractionArgs tapArgs) {
+        _isInteractive = true;
+      },
+      // Set to false when user touched the chart and hidden the visible Trackball.
+      onChartTouchInteractionUp: (ChartTouchInteractionArgs tapArgs) {
+        _isInteractive = false;
+        trackBall2.hide();
+      },
+      // Displayed the trackball when tap on the chart.
+      onTrackballPositionChanging: (TrackballArgs trackballArgs) {
+        if (_isInteractive) {
+          _secondPosition = _secondChartController!.pointToPixel(
+            trackballArgs.chartPointInfo.chartPoint!,
+          );
+          trackBall2.show(_secondPosition!.dx, _secondPosition!.dy, 'pixel');
+        }
+      },
+      backgroundColor: Colors.white,
+      primaryXAxis: const CategoryAxis(),
+      title: const ChartTitle(text: 'Chart 1'),
+      trackballBehavior: trackBall1,
+      series: <LineSeries<SalesData, String>>[
+        LineSeries<SalesData, String>(
+          dataSource: <SalesData>[
+            SalesData('Jan', 21),
+            SalesData('Feb', 24),
+            SalesData('Mar', 35),
+            SalesData('Apr', 38),
+            SalesData('May', 54),
+            SalesData('Jun', 21),
+            SalesData('Jul', 24),
+            SalesData('Aug', 35),
+            SalesData('Sep', 38),
+            SalesData('Oct', 54),
+            SalesData('Nov', 38),
+            SalesData('Dec', 54)
+          ],
+          xValueMapper: (SalesData sales, _) => sales.year,
+          yValueMapper: (SalesData sales, _) => sales.sales,
+          onRendererCreated: (ChartSeriesController controller) {
+            _firstChartController = controller;
+          },
+        ),
+      ],
+    );
   }
 }
 
 class SecondChart extends StatefulWidget {
+  const SecondChart({super.key});
+
   @override
   State<StatefulWidget> createState() {
     return SecondChartState();
@@ -100,35 +141,58 @@ class SecondChart extends StatefulWidget {
 }
 
 class SecondChartState extends State<SecondChart> {
+  // Declaring a field to check whether the user is interacting with the chart.
+  bool _isInteractive = false;
+
   @override
   Widget build(BuildContext context) {
     return SfCartesianChart(
-        onChartTouchInteractionMove: (ChartTouchInteractionArgs args) {
-          trackBall1.show(args.position.dx, args.position.dy, 'pixel');
-        },
-        backgroundColor: Colors.white,
-        primaryXAxis: CategoryAxis(),
-        title: ChartTitle(text: 'Chart 2'),
-        trackballBehavior: trackBall2,
-        series: <LineSeries<SalesData, String>>[
-          LineSeries<SalesData, String>(
-              dataSource: <SalesData>[
-                SalesData('Jan', 21),
-                SalesData('Feb', 24),
-                SalesData('Mar', 35),
-                SalesData('Apr', 38),
-                SalesData('May', 54),
-                SalesData('Jun', 21),
-                SalesData('Jul', 24),
-                SalesData('Aug', 35),
-                SalesData('Sep', 38),
-                SalesData('Oct', 54),
-                SalesData('Nov', 38),
-                SalesData('Dec', 54)
-              ],
-              xValueMapper: (SalesData sales, _) => sales.year,
-              yValueMapper: (SalesData sales, _) => sales.sales)
-        ]);
+      // Set to false when user touched the chart and hidden the visible Trackball.
+      onChartTouchInteractionDown: (ChartTouchInteractionArgs tapArgs) {
+        _isInteractive = true;
+      },
+      // Set to false when user touched the chart and hidden the visible Trackball.
+      onChartTouchInteractionUp: (ChartTouchInteractionArgs tapArgs) {
+        _isInteractive = false;
+        trackBall1.hide();
+      },
+      // Displayed the trackball when tap on the chart.
+      onTrackballPositionChanging: (TrackballArgs trackballArgs) {
+        if (_isInteractive) {
+          _firstPosition = _firstChartController!.pointToPixel(
+            trackballArgs.chartPointInfo.chartPoint!,
+          );
+          trackBall1.show(_firstPosition!.dx, _firstPosition!.dy, 'pixel');
+        }
+      },
+      backgroundColor: Colors.white,
+      primaryXAxis: const CategoryAxis(),
+      title: const ChartTitle(text: 'Chart 2'),
+      trackballBehavior: trackBall2,
+      series: <LineSeries<SalesData, String>>[
+        LineSeries<SalesData, String>(
+          dataSource: <SalesData>[
+            SalesData('Jan', 21),
+            SalesData('Feb', 24),
+            SalesData('Mar', 35),
+            SalesData('Apr', 38),
+            SalesData('May', 54),
+            SalesData('Jun', 21),
+            SalesData('Jul', 24),
+            SalesData('Aug', 35),
+            SalesData('Sep', 38),
+            SalesData('Oct', 54),
+            SalesData('Nov', 38),
+            SalesData('Dec', 54)
+          ],
+          xValueMapper: (SalesData sales, _) => sales.year,
+          yValueMapper: (SalesData sales, _) => sales.sales,
+          onRendererCreated: (ChartSeriesController controller) {
+            _secondChartController = controller;
+          },
+        ),
+      ],
+    );
   }
 }
 
